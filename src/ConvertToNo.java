4f51de56a06acfaaabe4478792cdb88e498d7350
sapan shah <nidhisoftwaretesting@gmail.com>
2019-12-15 14:38:19 +0000

    homework done

---
+++ src/ConvertToNo.java
@@ -0,0 +1,22 @@
+
+//program for - Convert string into number
+public class ConvertToNo {
+
+    //method to convert string into number
+
+    public static void StringToNumber(String price1, String price2) {
+        int total;
+        total = Integer.parseInt(price1) + Integer.parseInt(price2);
+        System.out.println("total of " + price1 + " and " + price2 + " is: " + total);
+    }
+
+    //main method
+
+    public static void main(String[] args) {
+
+        StringToNumber("100", "200");       //calling StringToNumber method using string as parameter
+
+
+    }
+}
+
